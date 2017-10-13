bfbc3829d2c287add265868ef12c9374b52769fb
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-10-13 15:25:40 -0700

    Finished HashSet exercises

--- hash_map/lib/p03_hash_set.rb
+++ hash_map/lib/p03_hash_set.rb
@@ -9,18 +9,28 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count >= num_buckets
+    unless include?(key)
+      @count += 1
+      self[key] << key
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      @count -= 1
+      self[key].delete(key)
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +38,11 @@ class HashSet
   end
 
   def resize!
+    flattened_store = @store.flatten
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    flattened_store.each do |el|
+      insert(el)
+    end
   end
 end
