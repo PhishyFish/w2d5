5e227b49026f97e821372b5b49c55164f1c58176
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-10-13 17:59:21 -0700

    Filled out LinkedList class

--- hash_map/lib/p04_linked_list.rb
+++ hash_map/lib/p04_linked_list.rb
@@ -19,7 +19,14 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @sentinel_head = Node.new
+    @sentinel_tail = Node.new
+
+    @sentinel_head.next = @sentinel_tail
+    @sentinel_tail.prev = @sentinel_head
   end
 
   def [](i)
@@ -28,30 +35,64 @@ class LinkedList
   end
 
   def first
+    @sentinel_head.next
   end
 
   def last
+    @sentinel_tail.prev
   end
 
   def empty?
+    @sentinel_head.next == @sentinel_tail
   end
 
   def get(key)
+    select { |node| node.key == key }
   end
 
   def include?(key)
+    each { |node| return true if node.key == key }
+
+    false
   end
 
   def append(key, val)
+    if include?(key)
+      update(key, val)
+    else
+      new_node = Node.new(key, val)
+      new_node.prev = last
+      last.next = new_node
+      new_node.next = @sentinel_tail
+      @sentinel_tail.prev = new_node
+    end
   end
 
   def update(key, val)
+    node = get(key)
+    node.value = val
   end
 
   def remove(key)
+    if include?(key)
+      current_node = get(key)
+      prev_node = current_node.prev
+      next_node = current_node.next
+
+      prev_node.next = next_node
+      next_node.prev = prev_node
+    end
   end
 
   def each
+    i = first
+
+    until i.next == @sentinel_tail
+      yield(i)
+      i = i.next
+    end
+
+    self
   end
 
   # uncomment when you have `each` working and `Enumerable` included
